d9897f7d88ed12bf416dfe368e7dc772a0050873
sushilv3 <sushilv3rma@gmail.com>
2021-05-19 10:59:28 +0530

    created common component

---
+++ src/components/Common.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import {NavLink} from "react-router-dom";
+import web from "../images/home.svg";
+
+const Common = (props) => {
+  return (
+    <>
+      <section id="header" className="d-flex align-items-center">
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-10 mx-auto">
+              <div className="row">
+                <div className="col-md-6 pt-6 pt-lg-0 order-2 order-lg-1 d-flex justify-content-center flex-column">
+                  <h1>
+                   {props.name}
+                    <strong className="brand-name">KodeCamp</strong>
+                  </h1>
+                  <h2 className="my-3">
+                    Lorem ipsum is placeholder text commonly used in the
+                    graphic, print, 
+                  </h2>
+                  <div className="my-3">
+                    <NavLink to={props.visit} className="btn-get-started">
+                      {props.btnname}
+                    </NavLink>
+                  </div>
+                </div>
+
+                <div className="col-lg-6 order-1 order-lg-2 header-img">
+                  <img
+                    src={props.imgsrc}
+                    className="img-fluid animated"
+                    alt="home img"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Common;
