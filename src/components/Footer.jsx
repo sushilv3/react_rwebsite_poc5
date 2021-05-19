37d2f8252713cb4ebfe6d405fdd62d4f5d649d36
sushilv3 <sushilv3rma@gmail.com>
2021-05-19 11:03:38 +0530

    created footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (<>
+    <footer className="text-center"> 
+    <p>
+    &copy; kodecamp.in. All rights reserved.
+    </p>
+    </footer>
+    </>);
+};
+
+export default Footer;
