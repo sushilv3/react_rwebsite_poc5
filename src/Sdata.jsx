9115dee91f847bd83560dc546f6f9ece2d480561
sushilv3 <sushilv3rma@gmail.com>
2021-05-19 10:57:44 +0530

    created dummy json data for cards

---
+++ src/Sdata.jsx
@@ -0,0 +1,71 @@
+import web from "./images/home.svg";
+
+const Sdata = [
+  {
+    imgsrc: web,
+    title: "web development",
+    about: "this Course has 10 module and 2 project",
+    visit: "/contact"
+  },
+  {
+    imgsrc: web,
+    title: "software development",
+    about: "this Course has 6 module and 1 project",
+    visit: "/contact"
+  },
+  {
+    imgsrc: web,
+    title: "SEO Course",
+    about: "this Course has 6 module and 1 project",
+    visit: "/contact"
+  },
+  {
+    imgsrc: web,
+    title: "Digital marketing",
+    about: "this Course has 12 module and 2 project",
+    visit: "/contact"
+  },
+  {
+    imgsrc: web,
+    title: "Native Application development",
+    about: "this Course has 15 module and 5 project",
+    visit: "/contact"
+  },
+  {
+    imgsrc: web,
+    title: "Android development",
+    about: "this Course has 15 module and 4 project",
+    visit: "/contact"
+  },
+  {
+    imgsrc: web,
+    title: "Java 8 Course",
+    about: "this Course has 20 module and 4 project",
+    visit: "/contact"
+  },
+  {
+    imgsrc: web,
+    title: "Spring web app development",
+    about: "this Course has 18 module and 5 project",
+    visit: "/contact"
+  },
+  {
+    imgsrc: web,
+    title: "DevOps Course",
+    about: "this Course has 5 module and 1 project",
+    visit: "/contact"
+  },
+  {
+    imgsrc: web,
+    title: "React & React Native Course",
+    about: "this Course has 10 module and 2 project",
+    visit: "/contact"
+  },
+  {
+    imgsrc: web,
+    title: "Fullter Course",
+    about: "this Course has 8 module and 1 project",
+    visit: "/contact"
+  }
+];
+export default Sdata;
