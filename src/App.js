3de7b5e28a5ebd5bdb2526a08e0fed2d05103ebd
sushilv3 <sushilv3rma@gmail.com>
2021-05-19 10:55:09 +0530

    added navBar and switch components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
+import Home from "./components/Home";
+import Service from "./components/Service";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Error from "./components/Error";
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+import { Redirect, Route, Switch } from "react-router-dom";
 
-function App() {
+const App = () => {
   return (
-   <>
-   <h2>App component</h2>
-   </>
+    <>
+    <NavBar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/service" component={Service} />
+        <Route exact path="/contact" component={Contact} />
+        {/**  <Route component={Error} /> */}
+        <Redirect to ="/" />
+      </Switch>
+      <Footer />
+    </>
   );
-}
+};
 
 export default App;
